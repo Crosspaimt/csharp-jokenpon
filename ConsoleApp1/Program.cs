8cbe4c2cd59fa403982fd5b01ccf11f9cc1c8420
Crosspaimt <davigames1905@gmail.com>
2023-08-01 20:55:53 -0300

    upload de jokenpon

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+
+
+public class jokenpon
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("1 = pedra");
+        Console.WriteLine("2 = papel");
+        Console.WriteLine("3 = tesoura");
+
+        Console.WriteLine("bota o numero");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Random random = new Random();
+        int num2 = random.Next(1, 4);
+        Console.WriteLine(num2);
+
+
+        if (num == num2)
+        { 
+
+            Console.WriteLine("empate");
+       
+        }
+        else { Console.WriteLine("perdeu"); }
+        
+        if (num2 == 2)
+        {
+
+            if (num == 3)
+            {
+
+                Console.WriteLine("ganhou");
+            }
+        }
+        if (num2 == 3)
+        {
+
+            if (num == 2)
+            {
+
+                Console.WriteLine("ganhou");
+            }
+        }
+       
+        if (num2 == 1)
+        {
+
+            if (num == 2)
+            {
+
+                Console.WriteLine("ganhou");
+            }          
+        }
+
+
+
+    }
+}
